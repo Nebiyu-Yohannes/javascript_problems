c69cde256d08624e153ffef0a4e1c0aeef06b49d
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-31 12:55:28 +0300

    SENOND Q - OPRIMIZED SOLUTION

--- Second.js
+++ Second.js
@@ -67,3 +67,42 @@ while (current) {
     console.log(current.val);
     current = current.next;
 }
+
+
+
+
+
+
+
+/* ANOTHE SOLUTION - SHORTER SOLUTION AND MORE OOPTIMIZED */
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode();
+    let current = dummy;
+    let carry = 0;
+
+    while (l1 || l2 || carry) {
+        const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    return dummy.next;
+};
