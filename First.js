9f1bd6aa7d5cde05211fb9a375b9ab1d2571f8d4
Nebiyu Yohannes <nebiyuyohannes2020@gmail.com>
2023-08-31 11:58:59 +0300

    first commit

---
+++ First.js
@@ -0,0 +1,30 @@
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+
+
+/**
+ * Finds two numbers in the given array that sum up to the target value.
+ * @param {number[]} nums - The array of numbers.
+ * @param {number} target - The target value.
+ * @return {number[]} - An array containing the indices of the two numbers.
+ */
+function twoSum(nums, target) {
+    // Iterate through each number in the array
+    for (let i = 0; i < nums.length; i++) {
+        // Iterate through each number after the current number
+        for (let j = i + 1; j < nums.length; j++) {
+            // Check if the sum of the current number and the next number is equal to the target
+            if (nums[i] + nums[j] === target) {
+                // Return the indices of the two numbers
+                return [i, j];
+            }
+        }
+    }
+}
